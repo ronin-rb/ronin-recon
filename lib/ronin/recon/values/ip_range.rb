f45d927a26338e13fb7e65c5d5e66adcbb401ea3
Postmodern <postmodern.mod3@gmail.com>
2023-04-15 21:12:08 -0700

    Added `Values::IPRange#as_json`.

--- lib/ronin/recon/values/ip_range.rb
+++ lib/ronin/recon/values/ip_range.rb
@@ -78,6 +78,16 @@ module Ronin
 
         alias to_str to_s
 
+        #
+        # Coerces the IP range value into JSON.
+        #
+        # @return [Hash{Symbol => Object}]
+        #   The Ruby Hash that will be converted into JSON.
+        #
+        def as_json
+          {type: :ip_range, range: @range}
+        end
+
       end
     end
   end
