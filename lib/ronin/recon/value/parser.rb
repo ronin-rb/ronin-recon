d42baabf5e31018ceeabb8fbdf58d879b00df877
Postmodern <postmodern.mod3@gmail.com>
2023-06-29 02:22:31 -0700

    Added `Value.parse` (issue #40).

---
+++ lib/ronin/recon/value/parser.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+#
+# ronin-recon - A micro-framework and tool for performing reconnaissance.
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-recon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-recon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-recon.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/recon/values/host'
+require 'ronin/recon/values/domain'
+require 'ronin/recon/values/wildcard'
+require 'ronin/recon/values/ip'
+require 'ronin/recon/values/ip_range'
+require 'ronin/recon/values/website'
+require 'ronin/recon/exceptions'
+
+require 'ronin/support/text/patterns/network'
+require 'ronin/support/network/public_suffix'
+
+require 'ipaddr'
+require 'uri'
+
+module Ronin
+  module Recon
+    #
+    # Module that parses strings into {Values::IP}, {Values::IPRange},
+    # {Values::Domain}, {Values::Host}, or {Values::Website}.
+    #
+    class Value
+
+      # Regular expression to match IPv4 and IPv6 addresses.
+      IP_REGEX = /\A#{Support::Text::Patterns::IP}\z/
+
+      # Regular expression to match IPv4 and IPv6 CIDR ranges.
+      IP_RANGE_REGEX = %r{\A(?:#{Support::Text::Patterns::IPV4_ADDR}/\d{1,2}|#{Support::Text::Patterns::IPV6_ADDR}/\d{1,3})\z}
+
+      # Regular expression to match sub-domain host-names.
+      HOSTNAME_REGEX = /\A(?:[a-zA-Z0-9_-]{1,63}\.)+#{Support::Text::Patterns::DOMAIN}\z/
+
+      # Regular expression to match domain host-names.
+      DOMAIN_REGEX = /\A#{Support::Text::Patterns::DOMAIN}\z/
+
+      # Regular expression to match wildcard host-names.
+      WILDCARD_REGEX = /\A\*(?:\.[a-z0-9_-]+)+\z/
+
+      # Regular expression to match https:// and http:// website base URLs.
+      WEBSITE_REGEX = %r{\Ahttp(?:s)?://[a-zA-Z0-9_-]+(?:\.[a-zA-Z0-9_-]+)*(?::\d+)?/?\z}
+
+      #
+      # Parses a value string.
+      #
+      # @param [String] string
+      #   The string to parse.
+      #
+      # @return [Values::IP, Values::IPRange, Values::Host, Values::Domain, Values::Wildcard, Values::Website]
+      #   The parsed value.
+      #
+      # @raise [UnknownValue]
+      #   Could not identify what value the string represents.
+      #
+      def self.parse(string)
+        case string
+        when IP_RANGE_REGEX then Values::IPRange.new(string)
+        when IP_REGEX       then Values::IP.new(string)
+        when WEBSITE_REGEX  then Values::Website.parse(string)
+        when WILDCARD_REGEX then Values::Wildcard.new(string)
+        when HOSTNAME_REGEX then Values::Host.new(string)
+        when DOMAIN_REGEX   then Values::Domain.new(string)
+        else
+          raise(UnknownValue,"unrecognized recon value: #{string.inspect}")
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/value/parser_spec.rb
@@ -0,0 +1,132 @@
+require 'spec_helper'
+require 'ronin/recon/value/parser'
+
+describe Ronin::Recon::Value do
+  describe ".parse" do
+    subject { described_class }
+
+    context "when given an IP CIDR range string" do
+      let(:string) { '1.2.3.4/24' }
+
+      it "must return a Values::IPRange object created with the string" do
+        value = subject.parse(string)
+
+        expect(value).to be_kind_of(Ronin::Recon::Values::IPRange)
+        expect(value.range).to eq(string)
+      end
+    end
+
+    context "when given an IP string" do
+      let(:string) { '1.2.3.4' }
+
+      it "must return a Values::IP object created with the string" do
+        value = subject.parse(string)
+
+        expect(value).to be_kind_of(Ronin::Recon::Values::IP)
+        expect(value.address).to eq(string)
+      end
+    end
+
+    context "when given website base URL" do
+      let(:host)   { 'example.com' }
+      let(:string) { "#{scheme}://#{host}" }
+
+      context "and the string starts with 'http://'" do
+        let(:scheme) { :http }
+
+        it "must return a Values::Website object with a :http scheme, host, and port of 443" do
+          value = subject.parse(string)
+
+          expect(value).to be_kind_of(Ronin::Recon::Values::Website)
+          expect(value.scheme).to be(scheme)
+          expect(value.host).to eq(host)
+          expect(value.port).to eq(80)
+        end
+
+        context "and the base URL contains a custom port" do
+          let(:port)   { 8080 }
+          let(:string) { "#{scheme}://#{host}:#{port}" }
+
+          it "must set the port" do
+            value = subject.parse(string)
+
+            expect(value).to be_kind_of(Ronin::Recon::Values::Website)
+            expect(value.scheme).to be(scheme)
+            expect(value.host).to eq(host)
+            expect(value.port).to eq(port)
+          end
+        end
+      end
+
+      context "and the string starts with 'https://'" do
+        let(:scheme) { :https }
+
+        it "must return a Values::Website object with a :https scheme, host, and port of 443" do
+          value = subject.parse(string)
+
+          expect(value).to be_kind_of(Ronin::Recon::Values::Website)
+          expect(value.scheme).to be(scheme)
+          expect(value.host).to eq(host)
+          expect(value.port).to eq(443)
+        end
+
+        context "and the base URL contains a custom port" do
+          let(:port)   { 8080 }
+          let(:string) { "#{scheme}://#{host}:#{port}" }
+
+          it "must set the port" do
+            value = subject.parse(string)
+
+            expect(value).to be_kind_of(Ronin::Recon::Values::Website)
+            expect(value.scheme).to be(scheme)
+            expect(value.host).to eq(host)
+            expect(value.port).to eq(port)
+          end
+        end
+      end
+    end
+
+    context "when given a wildcard hostname string" do
+      let(:string) { '*.example.com' }
+
+      it "must return a Values::Wildcard object created with the string" do
+        value = subject.parse(string)
+
+        expect(value).to be_kind_of(Ronin::Recon::Values::Wildcard)
+        expect(value.template).to eq(string)
+      end
+    end
+
+    context "when given a domain string" do
+      let(:string) { 'example.com' }
+
+      it "must return a Values::Domain object created with the string" do
+        value = subject.parse(string)
+
+        expect(value).to be_kind_of(Ronin::Recon::Values::Domain)
+        expect(value.name).to eq(string)
+      end
+    end
+
+    context "when given a hostname string" do
+      let(:string) { 'www.example.com' }
+
+      it "must return a Values::Host object created with the string" do
+        value = subject.parse(string)
+
+        expect(value).to be_kind_of(Ronin::Recon::Values::Host)
+        expect(value.name).to eq(string)
+      end
+    end
+
+    context "when given an unknown string" do
+      let(:string) { 'foo' }
+
+      it do
+        expect {
+          subject.parse(string)
+        }.to raise_error(Ronin::Recon::UnknownValue,"unrecognized recon value: #{string.inspect}")
+      end
+    end
+  end
+end
