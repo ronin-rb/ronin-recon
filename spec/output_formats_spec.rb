2865888e3c799a5cf29b363510a646c1ce4751ef
Postmodern <postmodern.mod3@gmail.com>
2023-07-03 18:10:50 -0700

    Added specs for `OutputFormats` (issue #38).

---
+++ spec/output_formats_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+require 'ronin/recon/output_formats'
+
+describe Ronin::Recon::OutputFormats do
+  describe "FORMATS" do
+    subject { described_class::FORMATS }
+
+    describe ":txt" do
+      it "must equal Ronin::Core::OutputFormats::TXT" do
+        expect(subject[:txt]).to be(Ronin::Core::OutputFormats::TXT)
+      end
+    end
+
+    describe ":csv" do
+      it "must equal Ronin::Core::OutputFormats::CSV" do
+        expect(subject[:csv]).to be(Ronin::Core::OutputFormats::CSV)
+      end
+    end
+
+    describe ":json" do
+      it "must equal Ronin::Core::OutputFormats::JSON" do
+        expect(subject[:json]).to be(Ronin::Core::OutputFormats::JSON)
+      end
+    end
+
+    describe ":ndjson" do
+      it "must equal Ronin::Core::OutputFormats::NDJSON" do
+        expect(subject[:ndjson]).to be(Ronin::Core::OutputFormats::NDJSON)
+      end
+    end
+
+    describe ":dir" do
+      it "must equal Ronin::Recon::OutputFormats::Dir" do
+        expect(subject[:dir]).to be(Ronin::Recon::OutputFormats::Dir)
+      end
+    end
+
+    describe ":dot" do
+      it "must equal Ronin::Recon::OutputFormats::Dot" do
+        expect(subject[:dot]).to be(Ronin::Recon::OutputFormats::Dot)
+      end
+    end
+  end
+
+  describe "FILE_EXTS" do
+    subject { described_class::FILE_EXTS }
+
+    describe "'.txt'" do
+      it "must equal Ronin::Core::OutputFormats::TXT" do
+        expect(subject['.txt']).to be(Ronin::Core::OutputFormats::TXT)
+      end
+    end
+
+    describe "'.csv'" do
+      it "must equal Ronin::Core::OutputFormats::CSV" do
+        expect(subject['.csv']).to be(Ronin::Core::OutputFormats::CSV)
+      end
+    end
+
+    describe "'.json'" do
+      it "must equal Ronin::Core::OutputFormats::JSON" do
+        expect(subject['.json']).to be(Ronin::Core::OutputFormats::JSON)
+      end
+    end
+
+    describe "'.ndjson'" do
+      it "must equal Ronin::Core::OutputFormats::NDJSON" do
+        expect(subject['.ndjson']).to be(Ronin::Core::OutputFormats::NDJSON)
+      end
+    end
+
+    describe "''" do
+      it "must equal Ronin::Recon::OutputFormats::Dir" do
+        expect(subject['']).to be(Ronin::Recon::OutputFormats::Dir)
+      end
+    end
+
+    describe "'.dot'" do
+      it "must equal Ronin::Recon::OutputFormats::Dot" do
+        expect(subject['.dot']).to be(Ronin::Recon::OutputFormats::Dot)
+      end
+    end
+  end
+end
