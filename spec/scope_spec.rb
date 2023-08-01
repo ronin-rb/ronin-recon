7569842883780606364cc6269a34efd55ea9ee91
AI-Mozi <bieniek.milosz0@gmail.com>
2023-08-01 08:50:18 +0200

    Style fixes

--- spec/scope_spec.rb
+++ spec/scope_spec.rb
@@ -3,16 +3,18 @@ require 'ronin/recon/scope'
 
 describe Ronin::Recon::Scope do
   subject { described_class.new(values) }
-  
+
   describe "#initialize" do
     context "for supported values" do
-      let(:values) { [
-        Ronin::Recon::Values::IP.new('1.2.3.4'),
-        Ronin::Recon::Values::IPRange.new('1.2.3.4/24'),
-        Ronin::Recon::Values::Domain.new('example.com'),
-        Ronin::Recon::Values::Host.new('www.example.com'),
-        Ronin::Recon::Values::Wildcard.new('*.example.com')
-      ] }
+      let(:values) do
+        [
+          Ronin::Recon::Values::IP.new('1.2.3.4'),
+          Ronin::Recon::Values::IPRange.new('1.2.3.4/24'),
+          Ronin::Recon::Values::Domain.new('example.com'),
+          Ronin::Recon::Values::Host.new('www.example.com'),
+          Ronin::Recon::Values::Wildcard.new('*.example.com')
+        ]
+      end
 
       it "must initialize #values" do
         expect(subject.values).to eq(values)
@@ -30,12 +32,12 @@ describe Ronin::Recon::Scope do
     end
   end
 
-  describe "include?" do
+  describe "#include?" do
     context "for value found in the scope" do
-      let(:ip) { Ronin::Recon::Values::IP.new('1.2.3.4') }
-      let(:host) { Ronin::Recon::Values::Host.new('www.example.com') }
+      let(:ip)     { Ronin::Recon::Values::IP.new('1.2.3.4') }
+      let(:host)   { Ronin::Recon::Values::Host.new('www.example.com') }
       let(:values) { [ip, host] }
-      
+
       it "must return true" do
         expect(subject.include?(ip)).to eq(true)
         expect(subject.include?(host)).to eq(true)
@@ -43,12 +45,14 @@ describe Ronin::Recon::Scope do
     end
 
     context "for value not found in the scope" do
-      let(:values) { [
-        Ronin::Recon::Values::IPRange.new('12.22.33.44/24'),
-        Ronin::Recon::Values::Wildcard.new('*.eexample.com')
-      ] }
-      
-      let(:ip) { Ronin::Recon::Values::IP.new('1.2.3.4') }
+      let(:values) do
+        [
+          Ronin::Recon::Values::IPRange.new('12.22.33.44/24'),
+          Ronin::Recon::Values::Wildcard.new('*.eexample.com')
+        ]
+      end
+
+      let(:ip)   { Ronin::Recon::Values::IP.new('1.2.3.4') }
       let(:host) { Ronin::Recon::Values::Host.new('www.example.com') }
 
       it "must return false" do
@@ -58,16 +62,18 @@ describe Ronin::Recon::Scope do
     end
 
     context "for value other than Host/Domain/IP/IPRange/Wildcard" do
-      let(:values) { [
-        Ronin::Recon::Values::IPRange.new('12.22.33.44/24'),
-        Ronin::Recon::Values::Wildcard.new('*.eexample.com')
-      ] }
+      let(:values) do
+        [
+          Ronin::Recon::Values::IPRange.new('12.22.33.44/24'),
+          Ronin::Recon::Values::Wildcard.new('*.eexample.com')
+        ]
+      end
 
-      let(:url) { 'https://www.example.com/index.html' }
+      let(:url)     { 'https://www.example.com/index.html' }
       let(:unknown) { Ronin::Recon::Values::URL.new(URI.parse(url)) }
-      
+
       it "must return true" do
-        expect(subject.include?(unknown)).to eq(true)  
+        expect(subject.include?(unknown)).to eq(true)
       end
     end
   end
