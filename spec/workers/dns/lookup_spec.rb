1effd8773fa2fe08a0a4fe14faeae35f905040aa
AI-Mozi <bieniek.milosz0@gmail.com>
2023-08-04 16:07:04 +0200

    Add specs for `DNS::Lookup`

---
+++ spec/workers/dns/lookup_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'ronin/recon/builtin/dns/lookup'
+
+describe Ronin::Recon::DNS::Lookup do
+  describe "#process" do
+    context "when there are IP addresses for the host" do
+      let(:host) { Ronin::Recon::Values::Host.new('www.example.com') }
+      
+      it "must yield them" do
+        yielded_values = []
+
+        Async do
+          subject.process(host) do |value|
+            yielded_values << value
+          end
+        end
+
+        expect(yielded_values).to eq("replace with ip")
+      end
+    end
+
+    context "when there is no IP address for the host" do
+      let(:host) { Ronin::Recon::Values::Host.new('www.example.com') }
+      
+      it "must not yield anything" do
+        expect { |b|
+          Async do
+            subject.process(host,&b)
+          end
+        }.not_to yield_control
+      end
+    end
+  end
+end
