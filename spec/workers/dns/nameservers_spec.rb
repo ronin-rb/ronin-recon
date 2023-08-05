7d4e782be46eb31572034b7d09c45fcc5117cc02
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-08-06 01:24:51 +0200

    Add specs for `DNS::Nameservers` (#55)

---
+++ spec/workers/dns/nameservers_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'ronin/recon/builtin/dns/nameservers'
+
+describe Ronin::Recon::DNS::Nameservers do
+  describe "#process" do
+    context "when there are nameservers for the domain" do
+      let(:domain)        { Ronin::Recon::Values::Domain.new('example.com') }
+      let(:nameserver)    { Ronin::Recon::Values::Nameserver.new('a.iana-servers.net') }
+      let(:nameserver2)   { Ronin::Recon::Values::Nameserver.new('b.iana-servers.net') }
+
+      it "must yield Nameserver values" do
+        yielded_values = []
+
+        Async do
+          subject.process(domain) do |value|
+            yielded_values << value
+          end
+        end
+
+        expect(yielded_values).to eq([nameserver, nameserver2])
+      end
+    end
+
+    context "when there is no nameserver for the domain" do
+      let(:domain) { Ronin::Recon::Values::Domain.new('localhost') }
+
+      it "must not yield anything" do
+        expect { |b|
+          Async do
+            subject.process(domain,&b)
+          end
+        }.not_to yield_control
+      end
+    end
+  end
+end
