b16ea383abc25108f2106f25bd072adc9a0e3a47
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-08-10 00:54:36 +0200

    Add `ValueStatus` specs (#64)

---
+++ spec/value_status_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+require 'ronin/recon/value_status'
+require 'ronin/recon/worker'
+
+describe Ronin::Recon::ValueStatus do
+  module TestValueStatus
+    class TestWorker1 < Ronin::Recon::Worker; end
+    class TestWorker2 < Ronin::Recon::Worker; end
+  end
+
+  let(:worker_class1) { TestValueStatus::TestWorker1 }
+  let(:worker_class2) { TestValueStatus::TestWorker2 }
+
+  describe "#initialize" do
+    it "must initialize #values" do
+      expect(subject.values).to eq({})
+    end
+  end
+
+  describe "#value_enqueued" do
+    let(:value) { Ronin::Recon::Values::IP.new('192.160.1.1') }
+
+    context "when #values has value key" do
+      before do
+        subject.value_enqueued(worker_class2, value)
+      end
+
+      it "must add new key worker_class with value :enqueued" do
+        subject.value_enqueued(worker_class1, value)
+
+        expect(subject.values[value]).to eq({ worker_class1 => :enqueued, worker_class2 => :enqueued })
+      end
+    end
+
+    context "when #values has no value key" do
+      it "must create an empty hash and add workerk_class key with value :enqueued" do
+        subject.value_enqueued(worker_class1, value)
+
+        expect(subject.values[value]).to eq({ worker_class1 => :enqueued })
+      end
+    end
+  end
+
+  describe "#job_started" do
+    let(:value) { Ronin::Recon::Values::IP.new('192.168.1.1') }
+
+    context "when #values has value key" do
+      before do
+        subject.value_enqueued(worker_class1, value)
+        subject.value_enqueued(worker_class2, value)
+      end
+
+      it "must add new key worker_class with value :enqueued" do
+        subject.job_started(worker_class1, value)
+
+        expect(subject.values[value]).to eq({ worker_class1 => :working, worker_class2 => :enqueued })
+      end
+    end
+
+    context "when #values has no value key" do
+      it "must create an empty hash and add workerk_class key with value :working" do
+        subject.job_started(worker_class1, value)
+
+        expect(subject.values[value]).to eq({ worker_class1 => :working })
+      end
+    end
+  end
+
+  describe "#job_completed" do
+    let(:value) { Ronin::Recon::Values::IP.new('162.168.1.1') }
+
+    context "when #values is empty" do
+      it "must retun nil" do
+        expect(subject.job_completed(worker_class1, value)).to be(nil)
+      end
+    end
+
+    context "when #values is not empty" do
+      context "and contains different workers" do
+        before do
+          subject.job_started(worker_class1, value)
+          subject.job_started(worker_class2, value)
+        end
+
+        it "must delete worker_class" do
+          subject.job_completed(worker_class1, value)
+
+          expect(subject.values[value]).to eq({ worker_class2 => :working })
+        end
+      end
+
+      context "but contains last worker" do
+        before do
+          subject.job_started(worker_class1, value)
+        end
+
+        it "must delete worker_class from value and value from #values" do
+          subject.job_completed(worker_class1, value)
+
+          expect(subject.values).to eq({})
+        end
+      end
+    end
+  end
+
+  describe "#empty?" do
+    context "when #values is empty" do
+      it "must return true" do
+        expect(subject.empty?).to be(true)
+      end
+    end
+
+    context "when #values is not empty" do
+      let(:value) { Ronin::Recon::Values::IP.new('192.168.1.1') }
+
+      it "must return false" do
+        subject.job_started(worker_class1, value)
+
+        expect(subject.empty?).to be(false)
+      end
+    end
+  end
+end
