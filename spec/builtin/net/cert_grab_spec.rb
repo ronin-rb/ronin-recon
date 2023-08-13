ea7923a1ab656c43befe9245a90c420e2afffd36
AI-Mozi <bieniek.milosz0@gmail.com>
2023-08-13 20:50:50 +0200

    Change variable name and path

--- spec/builtin/net/cert_grab_spec.rb
+++ spec/builtin/net/cert_grab_spec.rb
@@ -6,21 +6,21 @@ describe Ronin::Recon::Net::CertGrab do
     context "when there are certificates in the open port" do
       let(:port)  { Ronin::Recon::Values::OpenPort.new("93.184.216.34", 443, service: 'http', ssl: true) }
 
-      let(:fixtures_dir)  { File.expand_path(File.join(__dir__,'..','..','fixtures','values')) }
+      let(:fixtures_dir)  { File.expand_path(File.join(__dir__,'..','..','values','fixtures')) }
       let(:cert_path)     { File.join(fixtures_dir,'example.crt') }
       let(:cert)          { Ronin::Support::Crypto::Cert.load_file(cert_path) }
 
       it "must yield Cert" do
-        yielded_value = []
+        yielded_values = []
 
         Async do
           subject.process(port) do |value|
-            yielded_value << value
+            yielded_values << value
           end
         end
 
-        expect(yielded_value.size).to eq(1)
-        expect(yielded_valuei[0].cert).to eq(cert)
+        expect(yielded_values.size).to eq(1)
+        expect(yielded_values[0].cert).to eq(cert)
       end
     end
 
