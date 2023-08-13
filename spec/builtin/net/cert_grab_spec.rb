abf72a02582d352353c9fdc73d8ca1a8f1358ddd
AI-Mozi <bieniek.milosz0@gmail.com>
2023-08-13 20:48:24 +0200

    Change yielded values to array, remove redundant cert

--- spec/builtin/net/cert_grab_spec.rb
+++ spec/builtin/net/cert_grab_spec.rb
@@ -6,20 +6,21 @@ describe Ronin::Recon::Net::CertGrab do
     context "when there are certificates in the open port" do
       let(:port)  { Ronin::Recon::Values::OpenPort.new("93.184.216.34", 443, service: 'http', ssl: true) }
 
-      let(:fixtures_dir)  { File.expand_path(File.join(__dir__,'..','..','fixtures')) }
+      let(:fixtures_dir)  { File.expand_path(File.join(__dir__,'..','..','fixtures','values')) }
       let(:cert_path)     { File.join(fixtures_dir,'example.crt') }
       let(:cert)          { Ronin::Support::Crypto::Cert.load_file(cert_path) }
 
       it "must yield Cert" do
-        yielded_value = nil
+        yielded_value = []
 
         Async do
           subject.process(port) do |value|
-            yielded_value = value
+            yielded_value << value
           end
         end
 
-        expect(yielded_value.cert).to eq(cert)
+        expect(yielded_value.size).to eq(1)
+        expect(yielded_valuei[0].cert).to eq(cert)
       end
     end
 
