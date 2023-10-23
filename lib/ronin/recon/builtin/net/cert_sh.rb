0a1a8f9382c5e761367b1b4fdfcbac10643f0f3c
AI-Mozi <bieniek.milosz0@gmail.com>
2023-10-23 16:24:12 +0200

    implement CertSh worker

---
+++ lib/ronin/recon/builtin/net/cert_sh.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+#
+# ronin-recon - A micro-framework and tool for performing reconnaissance.
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-recon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-recon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-recon.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/recon/worker'
+
+require 'async/http/internet/instance'
+
+module Ronin
+  module Recon
+    module Net
+      #
+      # A recon worker that grabs the SSL/TLS certificate from open ports that
+      # use SSL/TLS.
+      #
+      class CertSh < Worker
+
+        register 'net/cert_sh'
+
+        accepts Domain
+
+        summary 'Returns host from each domains certificate.'
+
+        description <<~DESC
+          Returns host from each domains certificate.
+        DESC
+
+        #
+        # Returns host from each domains certificate.
+        #
+        # @param [Values::Domain] domain
+        #   The domain value to check.
+        #
+        # @yield [host]
+        #   If the domain has certificates, then a host value will be
+        #   yielded.
+        #
+        # @yieldparam [Values::Host] host
+        #   The host from certificate.
+        #
+        def process(host)
+          Async do
+            internet = Async::HTTP::Internet.instance
+            path = "https://crt.sh/?dNSName=#{host}&exclude=expired&output=json"
+
+            response = internet.get(path)
+            certs = JSON.parse(response.read, symbolize_names: true)
+
+            certs.each do |cert|
+              if cert[:common_name]
+                yield Host.new(name: cert[:common_name])
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
