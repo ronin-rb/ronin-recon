aad1ba87a0a6354387227d40e3c756c8971b6f47
Postmodern <postmodern.mod3@gmail.com>
2024-01-05 03:35:48 -0800

    Added the `ronin-recon irb` command (closes #105).

---
+++ lib/ronin/recon/cli/commands/irb.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+#
+# ronin-recon - A micro-framework and tool for performing reconnaissance.
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-recon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-recon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-recon.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/recon/cli/command'
+require 'ronin/recon/cli/ruby_shell'
+
+module Ronin
+  module Recon
+    class CLI
+      module Commands
+        #
+        # Starts an interactive Ruby shell with `ronin-recon` loaded.
+        #
+        # ## Usage
+        #
+        #     ronin-recon irb [options]
+        #
+        # ## Options
+        #
+        #     -h, --help                       Print help information
+        #
+        class Irb < Command
+
+          description "Starts an interactive Ruby shell with ronin-recon loaded"
+
+          man_page 'ronin-recon-irb.1'
+
+          #
+          # Runs the `ronin-recon irb` command.
+          #
+          def run
+            require 'ronin/recon'
+            CLI::RubyShell.start
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-recon-irb.1.md
@@ -0,0 +1,26 @@
+# ronin-recon-irb 1 "2023-02-01" Ronin "User Manuals"
+
+## NAME
+
+ronin-recon-irb - Starts an interactive Ruby shell with ronin-recon loaded
+
+## SYNOPSIS
+
+`ronin-recon irb` [*options*]
+
+## DESCRIPTION
+
+Starts an interactive Ruby shell with `ronin/recon` loaded.
+
+## OPTIONS
+
+`-h`, `--help`
+: Print help information
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+[ronin-recon-workers](ronin-recon-workers.1.md) [ronin-recon-worker](ronin-recon-worker.1.md) [ronin-recon-run](ronin-recon-run.1.md) [ronin-recon-test](ronin-recon-test.1.md)
