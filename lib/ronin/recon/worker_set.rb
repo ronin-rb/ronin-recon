ba388249e34b801f2b5600e5a1f91167fbe74d6a
Postmodern <postmodern.mod3@gmail.com>
2024-01-08 01:05:31 -0800

    Added the `WorkerSet` class (closes #85).

---
+++ lib/ronin/recon/worker_set.rb
@@ -0,0 +1,277 @@
+# frozen_string_literal: true
+#
+# ronin-recon - A micro-framework and tool for performing reconnaissance.
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-recon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-recon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-recon.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/recon/registry'
+
+require 'set'
+
+module Ronin
+  module Recon
+    #
+    # Represents a set of recon workers.
+    #
+    # @api semipublic
+    #
+    class WorkerSet
+
+      include Enumerable
+
+      # The workers in the set.
+      #
+      # @return [Set<Class<Worker>>]
+      attr_reader :workers
+
+      #
+      # Initializes the worker set.
+      #
+      # @param [Array<Class<Worker>>] workers
+      #   The array of worker classes.
+      #
+      def initialize(workers=Set.new)
+        @workers = workers.to_set
+      end
+
+      #
+      # Loads the worker classes.
+      #
+      # @param [Array<String>] worker_ids
+      #   The array of worker IDs to load.
+      #
+      # @return [WorkerSet]
+      #
+      def self.load(worker_ids)
+        worker_set = new
+
+        worker_ids.each do |worker_id|
+          worker_set.load(worker_id)
+        end
+
+        return worker_set
+      end
+
+      #
+      # Alias for {load}.
+      #
+      # @param [Array<String>] worker_ids
+      #   The array of worker IDs to load.
+      #
+      # @return [WorkerSet]
+      #
+      def self.[](*worker_ids)
+        load(worker_ids)
+      end
+
+      # The default set of workers to load.
+      #
+      # @api private
+      DEFAULT_SET = %w[
+        dns/lookup
+        dns/mailservers
+        dns/nameservers
+        dns/subdomain_enum
+        dns/suffix_enum
+        dns/srv_enum
+        net/ip_range_enum
+        net/port_scan
+        net/service_id
+        ssl/cert_grab
+        ssl/cert_enum
+        web/spider
+        web/dir_enum
+      ]
+
+      #
+      # Loads the default set of workers.
+      #
+      # * {DNS::Lookup dns/lookup}
+      # * {DNS::Mailservers dns/mailservers}
+      # * {DNS::Nameservers dns/nameservers}
+      # * {DNS::SubdomainEnum dns/subdomain_enum}
+      # * {DNS::SuffixEnum dns/suffix_enum}
+      # * {DNS::SRVEnum dns/srv_enum}
+      # * {Net::IPRangeEnum net/ip_range_enum}
+      # * {Net::PortScan net/port_scan}
+      # * {Net::ServiceID net/service_id}
+      # * {SSL::CertGrab ssl/cert_grab}
+      # * {SSL::CertEnum ssl/cert_enum}
+      # * {Web::Spider web/spider}
+      # * {Web::DirEnum web/dir_enum}
+      #
+      # @return [WorkerSet]
+      #
+      def self.default
+        load(DEFAULT_SET)
+      end
+
+      #
+      # Loads all workers.
+      #
+      # @return [WorkerSet]
+      #
+      def self.all
+        load(Recon.list_files)
+      end
+
+      #
+      # Loads all workers under a specific category.
+      #
+      # @param [String] name
+      #   The category name.
+      #
+      # @return [WorkerSet]
+      #
+      def self.category(name)
+        load(
+          Recon.list_files.select { |worker_id|
+            worker_id.start_with?("#{name}/")
+          }
+        )
+      end
+
+      #
+      # Enumerates over the worker classes within the set.
+      #
+      # @yield [worker]
+      #   If a block is given, it will be passed every worker class within the
+      #   set.
+      #
+      # @yieldparam [Class<Worker>] worker
+      #   A worker class within the set.
+      #
+      # @return [Enumerator]
+      #   If no block is given, an Enumerator object will be returned.
+      #
+      def each(&block)
+        @workers.each(&block)
+      end
+
+      #
+      # Adds another set of workers to the worker set.
+      #
+      # @param [WorkerSet, Array<Class<Worker>>] other
+      #
+      # @return [WorkerSet]
+      #
+      def +(other)
+        other_workers = other.to_set
+
+        self.class.new((@workers + other_workers).uniq)
+      end
+
+      #
+      # Adds the worker class to the worker set.
+      #
+      # @param [Class<Worker>] worker
+      #   The worker class to add.
+      #
+      # @return [self]
+      #
+      def <<(worker)
+        @workers << worker
+        return self
+      end
+
+      #
+      # Loads a worker and adds it to the worker set.
+      #
+      # @param [String] worker_id
+      #   The worker ID to load.
+      #
+      # @return [self]
+      #
+      def load(worker_id)
+        self << Recon.load_class(worker_id)
+      end
+
+      #
+      # Removes a worker class from the worker set.
+      #
+      # @param [Class<Worker>] worker
+      #   The worker class to remove.
+      #
+      # @return [self, nil]
+      #   If the worker class was in the worker set, than `self` is returned.
+      #   If the worker class was not in the worker set, then `nil` will be
+      #   returned.
+      #
+      def delete(worker)
+        if @workers.delete?(worker)
+          self
+        end
+      end
+
+      #
+      # Removes a worker with the ID from the worker set.
+      #
+      # @param [String] worker_id
+      #   The worker ID to remove.
+      #
+      # @return [self, nil]
+      #   If the worker ID was in the worker set, than `self` is returned.
+      #   If the worker ID was not in the worker set, then `nil` will be
+      #   returned.
+      #
+      def remove(worker_id)
+        if @workers.reject! { |worker| worker.id == worker_id }
+          self
+        end
+      end
+
+      # Intensity levels sorted by their intensity.
+      #
+      # @api private
+      INTENSITY_LEVELS = [
+        :passive,
+        :active,
+        :intense
+      ]
+
+      #
+      # Filters the workers by their {Worker.intensity intensity} level.
+      #
+      # @param [:passive, :active, :intensive] level
+      #   The maximum intensity level to filter by.
+      #
+      # @return [WorkerSet]
+      #
+      def intensity(level)
+        level_index = INTENSITY_LEVELS.index(level)
+
+        self.class.new(
+          @workers.select { |worker|
+            if (intensity_index = INTENSITY_LEVELS.index(worker.intensity))
+              intensity_index <= level_index
+            end
+          }
+        )
+      end
+
+      #
+      # Converts the worker set into a Set.
+      #
+      # @return [Set<Class<Worker>>]
+      #
+      def to_set
+        @workers
+      end
+
+    end
+  end
+end
---
+++ spec/worker_set_spec.rb
@@ -0,0 +1,280 @@
+require 'spec_helper'
+require 'ronin/recon/worker_set'
+require 'ronin/recon/builtin'
+
+describe Ronin::Recon::WorkerSet do
+  let(:worker_ids) do
+    %w[
+      dns/lookup
+      dns/mailservers
+      dns/nameservers
+      dns/subdomain_enum
+      dns/suffix_enum
+      dns/srv_enum
+      net/ip_range_enum
+      net/port_scan
+      net/service_id
+      ssl/cert_grab
+      ssl/cert_enum
+      web/spider
+    ]
+  end
+
+  let(:workers) do
+    [
+      Ronin::Recon::DNS::Lookup,
+      Ronin::Recon::DNS::Mailservers,
+      Ronin::Recon::DNS::Nameservers,
+      Ronin::Recon::DNS::SubdomainEnum,
+      Ronin::Recon::DNS::SuffixEnum,
+      Ronin::Recon::DNS::SRVEnum,
+      Ronin::Recon::Net::IPRangeEnum,
+      Ronin::Recon::Net::PortScan,
+      Ronin::Recon::Net::ServiceID,
+      Ronin::Recon::SSL::CertGrab,
+      Ronin::Recon::SSL::CertEnum,
+      Ronin::Recon::Web::Spider
+    ]
+  end
+
+  subject { described_class.new(workers) }
+
+  describe "#initialize" do
+    context "when given an Array of workers" do
+      it "must initialize #workers to a Set of the given workers" do
+        expect(subject.workers).to be_kind_of(Set)
+        expect(subject.workers.to_a).to eq(workers)
+      end
+    end
+
+    context "when no arguments are given" do
+      subject { described_class.new }
+
+      it "must initialize #workers to an empty Set" do
+        expect(subject.workers).to eq(Set.new)
+      end
+    end
+  end
+
+  describe ".load" do
+    subject { described_class }
+
+    it "must load the worker classes and return a new #{described_class}" do
+      worker_set = subject.load(worker_ids)
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.to_a).to eq(workers)
+    end
+  end
+
+  describe ".[]" do
+    subject { described_class }
+
+    it "must load the worker classes and return a new #{described_class}" do
+      worker_set = subject[*worker_ids]
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.to_a).to eq(workers)
+    end
+  end
+
+  describe ".all" do
+    subject { described_class }
+
+    it "must load all workers and return a #{described_class}" do
+      worker_set = subject.all
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.map(&:id)).to eq(
+        Ronin::Recon.list_files
+      )
+    end
+  end
+
+  describe ".default" do
+    subject { described_class }
+
+    it "must load the default set of workers and return a #{described_class}" do
+      worker_set = subject.default
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.map(&:id)).to eq(described_class::DEFAULT_SET)
+    end
+  end
+
+  describe ".category" do
+    subject { described_class }
+
+    let(:category) { 'dns' }
+
+    it "must load the workers within the cateogyr and return a #{described_class}" do
+      worker_set = subject.category(category)
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.map(&:id)).to all(start_with(category))
+    end
+  end
+
+  describe "#each" do
+    context "when given a block" do
+      it "must enumerate over the worker classes in #workers" do
+        expect { |b|
+          subject.each(&b)
+        }.to yield_successive_args(*workers)
+      end
+    end
+
+    context "when no block is given" do
+      it "must return an Enumerator for the workers classes in #workers" do
+        expect(subject.each).to be_kind_of(Enumerator)
+        expect(subject.each.to_a).to eq(workers)
+      end
+    end
+  end
+
+  describe "#+" do
+    let(:workers) do
+      [
+        Ronin::Recon::DNS::Lookup,
+        Ronin::Recon::DNS::Mailservers,
+        Ronin::Recon::DNS::Nameservers,
+        Ronin::Recon::DNS::SubdomainEnum,
+        Ronin::Recon::DNS::SuffixEnum,
+        Ronin::Recon::DNS::SRVEnum,
+        Ronin::Recon::Net::IPRangeEnum,
+        Ronin::Recon::Net::PortScan,
+        Ronin::Recon::Net::ServiceID,
+        Ronin::Recon::SSL::CertGrab,
+        Ronin::Recon::SSL::CertEnum
+      ]
+    end
+
+    subject { described_class.new(workers) }
+
+    let(:other_workers) do
+      [
+        Ronin::Recon::Net::IPRangeEnum,
+        Ronin::Recon::Net::PortScan,
+        Ronin::Recon::Net::ServiceID,
+        Ronin::Recon::SSL::CertGrab,
+        Ronin::Recon::SSL::CertEnum,
+        Ronin::Recon::Web::Spider
+      ]
+    end
+
+    let(:other_worker_set) { described_class.new(other_workers) }
+
+    context "when given another #{described_class}" do
+      it "must combine the workers in the other worker set with the worker set" do
+        worker_set = subject + other_worker_set
+
+        expect(worker_set).to be_kind_of(described_class)
+        expect(worker_set.workers).to eq(
+          (workers + other_workers).to_set
+        )
+      end
+    end
+
+    context "when given an Array of workers" do
+      let(:other_worker_set) { other_workers }
+
+      it "must combine the workers in the other worker set with the worker set" do
+        worker_set = subject + other_worker_set
+
+        expect(worker_set).to be_kind_of(described_class)
+        expect(worker_set.workers).to eq(
+          (workers + other_workers).to_set
+        )
+      end
+    end
+  end
+
+  describe "#<<" do
+    let(:worker) { Ronin::Recon::Web::DirEnum }
+
+    before { subject << worker }
+
+    it "must add the worker class to the worker set" do
+      expect(subject.workers).to include(worker)
+    end
+  end
+
+  describe "#load" do
+    let(:worker_id) { 'web/dir_enum' }
+    let(:worker)    { Ronin::Recon::Web::DirEnum }
+
+    before { subject.load(worker_id) }
+
+    it "must load and add the worker class to the worker set" do
+      expect(subject.workers).to include(worker)
+    end
+  end
+
+  describe "#delete" do
+    context "when the worker class does exist in the worker set" do
+      let(:worker) { Ronin::Recon::DNS::SubdomainEnum }
+
+      it "must retruen self" do
+        expect(subject.delete(worker)).to be(subject)
+      end
+
+      it "must remove the worker class from the worker set" do
+        subject.delete(worker)
+
+        expect(subject.workers).to_not include(worker)
+      end
+    end
+
+    context "when the worker class does not exist in the worker set" do
+      let(:worker) { Ronin::Recon::Web::DirEnum }
+
+      it "must return nil" do
+        expect(subject.delete(worker)).to be(nil)
+      end
+    end
+  end
+
+  describe "#remove" do
+    context "when the worker ID exists within the worker set" do
+      let(:worker)    { Ronin::Recon::DNS::SubdomainEnum }
+      let(:worker_id) { 'dns/subdomain_enum' }
+
+      it "must retruen self" do
+        expect(subject.remove(worker_id)).to be(subject)
+      end
+
+      it "must remove the worker class from the worker set" do
+        subject.remove(worker_id)
+
+        expect(subject.workers).to_not include(worker)
+      end
+    end
+
+    context "when the worker ID does not exist in the worker set" do
+      let(:worker)    { Ronin::Recon::Web::DirEnum }
+      let(:worker_id) { 'web/dir_enum' }
+
+      it "must return nil" do
+        expect(subject.remove(worker_id)).to be(nil)
+      end
+    end
+  end
+
+  describe "#intensity" do
+    let(:intensity) { :active }
+
+    it "must filter the workers by intensity and return a new #{described_class}" do
+      worker_set = subject.intensity(intensity)
+
+      expect(worker_set).to be_kind_of(described_class)
+      expect(worker_set.workers.map(&:intensity)).to all(eq(:passive).or(eq(:active)))
+    end
+  end
+
+  describe "#to_set" do
+    it "must return a Set of the workers" do
+      expect(subject.to_set).to be_kind_of(Set)
+      expect(subject.to_set.to_a).to eq(workers)
+    end
+  end
+end
