bbc265f1b1f281ab1899777d0bfd91b4b9b449b6
Postmodern <postmodern.mod3@gmail.com>
2024-06-07 19:20:55 -0700

    Use `http://testphp.vulnweb.com` for the `Web::Spider` network specs.

--- spec/builtin/web/spider_spec.rb
+++ spec/builtin/web/spider_spec.rb
@@ -379,15 +379,62 @@ describe Ronin::Recon::Web::Spider do
     before(:all) { WebMock.allow_net_connect! }
 
     context "when there are URLs in the website" do
-      let(:website) { Ronin::Recon::Values::Website.new(:https, "www.google.com", 443) }
+      let(:website) do
+        Ronin::Recon::Values::Website.http("testphp.vulnweb.com")
+      end
+
       let(:expected_uris) do
-        [
-          "https://www.google.com/",
-          "https://www.google.com/preferences?hl=pl",
-          "https://www.google.com/advanced_search?hl=pl&authuser=0",
-          "https://www.google.com/intl/pl/policies/privacy/",
-          "https://www.google.com/intl/pl/policies/terms/",
-          "https://www.google.com/xjs/_/ss/k=xjs.hp.Rf6tYsQEqH4.L.X.O/am=AQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAEMgAAAAAAEAAAAIgAAIAg/d=1/ed=1/rs=ACT90oEwkAntX8xwkxuouRQvNEvlrRcUvQ/m=sb_he,d"
+        %w[
+          http://testphp.vulnweb.com/
+          http://testphp.vulnweb.com/index.php
+          http://testphp.vulnweb.com/categories.php
+          http://testphp.vulnweb.com/artists.php
+          http://testphp.vulnweb.com/disclaimer.php
+          http://testphp.vulnweb.com/cart.php
+          http://testphp.vulnweb.com/guestbook.php
+          http://testphp.vulnweb.com/AJAX/index.php
+          http://testphp.vulnweb.com/login.php
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/
+          http://testphp.vulnweb.com/hpp/
+          http://testphp.vulnweb.com/style.css
+          http://testphp.vulnweb.com/listproducts.php?cat=1
+          http://testphp.vulnweb.com/listproducts.php?cat=2
+          http://testphp.vulnweb.com/listproducts.php?cat=3
+          http://testphp.vulnweb.com/listproducts.php?cat=4
+          http://testphp.vulnweb.com/artists.php?artist=1
+          http://testphp.vulnweb.com/artists.php?artist=2
+          http://testphp.vulnweb.com/artists.php?artist=3
+          http://testphp.vulnweb.com/AJAX/showxml.php
+          http://testphp.vulnweb.com/AJAX/styles.css
+          http://testphp.vulnweb.com/signup.php
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/Details/network-attached-storage-dlink/1/
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/Details/web-camera-a4tech/2/
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/Details/color-printer/3/
+          http://testphp.vulnweb.com/hpp/?pp=12
+          http://testphp.vulnweb.com/product.php?pic=1
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/1.jpg
+          http://testphp.vulnweb.com/product.php?pic=2
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/2.jpg
+          http://testphp.vulnweb.com/product.php?pic=3
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/3.jpg
+          http://testphp.vulnweb.com/product.php?pic=4
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/4.jpg
+          http://testphp.vulnweb.com/product.php?pic=5
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/5.jpg
+          http://testphp.vulnweb.com/product.php?pic=7
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/7.jpg
+          http://testphp.vulnweb.com/product.php?pic=6
+          http://testphp.vulnweb.com/showimage.php?file=./pictures/6.jpg
+          http://testphp.vulnweb.com/listproducts.php?artist=1
+          http://testphp.vulnweb.com/listproducts.php?artist=2
+          http://testphp.vulnweb.com/listproducts.php?artist=3
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/BuyProduct-1/
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/RateProduct-1.html
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/BuyProduct-2/
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/RateProduct-2.html
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/BuyProduct-3/
+          http://testphp.vulnweb.com/Mod_Rewrite_Shop/RateProduct-3.html
+          http://testphp.vulnweb.com/hpp/params.php?p=valid&pp=12
         ]
       end
 
@@ -398,7 +445,6 @@ describe Ronin::Recon::Web::Spider do
           yielded_values << value
         end
 
-        expect(yielded_values.size).to eq(6)
         expect(yielded_values).to all(be_kind_of(Ronin::Recon::Values::URL))
         expect(yielded_values.map(&:uri).map(&:to_s)).to match_array(expected_uris)
         expect(yielded_values.map(&:status)).to all(eq(200))
