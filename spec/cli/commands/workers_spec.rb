507808d4ae9d7311a04c8ade854642d39e45e6b9
Postmodern <postmodern.mod3@gmail.com>
2024-09-05 22:09:55 -0700

    Show which workers are enabled in `ronin-recon workers` (closes #175).

--- spec/cli/commands/workers_spec.rb
+++ spec/cli/commands/workers_spec.rb
@@ -1,15 +1,39 @@
 require 'spec_helper'
 require 'ronin/recon/cli/commands/workers'
+require 'ronin/recon/config'
 
 describe Ronin::Recon::CLI::Commands::Workers do
   describe "#run" do
+    let(:config) { Ronin::Recon::Config.default }
+
     it "must list all worker IDs" do
       expect {
         subject.run
       }.to output(
-        Ronin::Recon.list_files.map { |id|
-          "  #{id}"
-        }.join($/) + $/
+        [
+          "  api/built_with                    ",
+          "  api/crt_sh                        ",
+          "  api/hunter_io                     ",
+          "  api/security_trails               ",
+          "  api/zoom_eye                      ",
+          "  dns/lookup             [enabled]  ",
+          "  dns/mailservers        [enabled]  ",
+          "  dns/nameservers        [enabled]  ",
+          "  dns/reverse_lookup     [enabled]  ",
+          "  dns/srv_enum           [enabled]  ",
+          "  dns/subdomain_enum     [enabled]  ",
+          "  dns/suffix_enum        [enabled]  ",
+          "  net/ip_range_enum      [enabled]  ",
+          "  net/port_scan          [enabled]  ",
+          "  net/service_id         [enabled]  ",
+          "  ssl/cert_enum          [enabled]  ",
+          "  ssl/cert_grab          [enabled]  ",
+          "  web/dir_enum           [enabled]  ",
+          "  web/email_addresses    [enabled]  ",
+          "  web/screenshot                    ",
+          "  web/spider             [enabled]  ",
+          ""
+        ].join($/)
       ).to_stdout
     end
 
@@ -20,11 +44,16 @@ describe Ronin::Recon::CLI::Commands::Workers do
         expect {
           subject.run(dir)
         }.to output(
-          Ronin::Recon.list_files.select { |id|
-            id.start_with?("#{dir}/")
-          }.map { |id|
-            "  #{id}"
-          }.join($/) + $/
+          [
+            "  dns/lookup            [enabled]  ",
+            "  dns/mailservers       [enabled]  ",
+            "  dns/nameservers       [enabled]  ",
+            "  dns/reverse_lookup    [enabled]  ",
+            "  dns/srv_enum          [enabled]  ",
+            "  dns/subdomain_enum    [enabled]  ",
+            "  dns/suffix_enum       [enabled]  ",
+            ""
+          ].join($/)
         ).to_stdout
       end
     end
