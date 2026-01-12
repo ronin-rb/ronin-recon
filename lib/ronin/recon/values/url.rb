abcd4e89bedbdb5d08fbafca3794ec3b85fe5d9a
Postmodern <postmodern.mod3@gmail.com>
2026-01-11 21:05:29 -0800

    Updated the copyright years.

--- lib/ronin/recon/values/url.rb
+++ lib/ronin/recon/values/url.rb
@@ -2,7 +2,7 @@
 #
 # ronin-recon - A micro-framework and tool for performing reconnaissance.
 #
-# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
+# Copyright (c) 2023-2026 Hal Brodigan (postmodern.mod3@gmail.com)
 #
 # ronin-recon is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
